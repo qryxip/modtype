998ea724ab01aa55f5aee6432260cb268148998c
Ryo Yamashita <qryxip@gmail.com>
2019-07-03 16:01:20 +0900

    Add a test for std::cmp trait implementations

---
+++ tests/cmp.rs
@@ -0,0 +1,22 @@
+use modtype::{use_modtype, DefaultModType};
+
+#[test]
+fn cmp() {
+    #[use_modtype]
+    type F = DefaultModType<5u32>;
+
+    for a in 0..=4 {
+        for b in 0..=4 {
+            assert_eq!(a == b, F(a) == F(b));
+            assert_eq!(a != b, F(a) != F(b));
+            assert_eq!(a.partial_cmp(&b), F(a).partial_cmp(&F(b)));
+            assert_eq!(a < b, F(a) < F(b));
+            assert_eq!(a <= b, F(a) <= F(b));
+            assert_eq!(a > b, F(a) > F(b));
+            assert_eq!(a >= b, F(a) >= F(b));
+            assert_eq!(a.cmp(&b), F(a).cmp(&F(b)));
+            assert_eq!(a.min(b), *F(a).min(F(b)));
+            assert_eq!(a.max(b), *F(a).max(F(b)));
+        }
+    }
+}
